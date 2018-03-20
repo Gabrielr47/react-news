2f56cf350a8f1d894ba85f5b01bb1049976ed07b
Gabriel Rodrigues <gabrielr47@gmail.com>
2018-03-20 03:18:28 -0300

    fallback img

--- src/components/PostItem.js
+++ src/components/PostItem.js
@@ -16,7 +16,7 @@ class Post extends Component {
             return (
                 <div className="col-md-3" key={i}>
                     <div className="card">
-                        <img className="card-img-top" src={p.urlToImage} alt="Card cap"/>
+                        <img className="card-img-top" src={p.urlToImage || 'img/img-not-found.png'} alt="Card cap"/>
                         <div className="card-body">
                             <h5 className="card-title">{p.title}</h5>
                             <p className="card-text">{p.description}</p>
